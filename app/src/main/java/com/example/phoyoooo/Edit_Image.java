28fd2f41e8de98504c6f76ec64bdd03c2298a9fc
yatsenko.aa2201 <adlik2033@yandex.ru>
2025-10-27 17:11:39 +0300

    000

--- app/src/main/java/com/example/phoyoooo/Edit_Image.java
+++ app/src/main/java/com/example/phoyoooo/Edit_Image.java
@@ -1,16 +1,17 @@
 package com.example.phoyoooo;
 
+import android.graphics.Bitmap;
+import android.graphics.Matrix;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ImageView;
 
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
 public class Edit_Image extends AppCompatActivity {
 
+    Bitmap selectedImage;
+    ImageView photoView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -21,6 +22,34 @@ public class Edit_Image extends AppCompatActivity {
 
     private void Init(){
         ImageView photo = findViewById(R.id.imageView);
+        selectedImage = MainActivity.selectedImage;
         photo.setImageBitmap(MainActivity.selectedImage);
+    }
+
+    public void Rotate90L(View view){
+        Matrix matrix = new Matrix();
+        matrix.setRotate((float) 90);
+        selectedImage = Bitmap.createBitmap(selectedImage, 0, 0, selectedImage.getWidth(), selectedImage.getHeight(), matrix, false);
+        photoView.setImageBitmap(selectedImage);
+    }
+
+    public void ReflectRTL(View view){
+        Matrix matrix = new Matrix();
+        matrix.setScale(-1, 1);
+        selectedImage = Bitmap.createBitmap(selectedImage, 0, 0, selectedImage.getWidth(), selectedImage.getHeight(), matrix, false);
+        photoView.setImageBitmap(selectedImage);
+    }
+    public void Rotate90R(View view){
+        Matrix matrix = new Matrix();
+        matrix.setRotate((float) 270);
+        selectedImage = Bitmap.createBitmap(selectedImage, 0, 0, selectedImage.getWidth(), selectedImage.getHeight(), matrix, false);
+        photoView.setImageBitmap(selectedImage);
+    }
+
+    public void ReflectTTB(View view){
+        Matrix matrix = new Matrix();
+        matrix.setScale(-1f, 1f, selectedImage.getWidth(), selectedImage.getHeight());
+        selectedImage = Bitmap.createBitmap(selectedImage, 0, 0, selectedImage.getWidth(), selectedImage.getHeight(), matrix, false);
+        photoView.setImageBitmap(selectedImage);
     }
 }
